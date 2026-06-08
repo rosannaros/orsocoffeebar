22cd1352b7cd95d9b6039cac6206186e491b95e3
afrahmrtza <afrahmaritza18@gmail.com>
2026-06-08 10:58:33 +0800

    update

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Bulan Mei 2026 pada 07.38
+-- Waktu pembuatan: 08 Jun 2026 pada 04.57
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -1090,6 +1090,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `role`) VALUES
 (1, 'Afrah', 'afrah@gmail.com', '$2y$10$xjq4eEtVge8JCTp8xnJfpuPBU4IZeb/92SLjT8N4NZ7hs9hMhuQla', 'pemilik'),
+(6, 'roje123', 'roje12@gmail.com', '$2y$10$rSMkaZuCH4M7qs9vE1nAfueIn/IbpMXjTanz9Sg98kC8FppC0mqb.', 'pelanggan'),
 (7, 'maritza', 'maritza@gmail.com', '$2y$10$bh4e1wSaRkNt6yEah7U2JebDKXf0GqLW557LapnNt/TDxc14TEJXi', 'kasir'),
 (8, 'inaa', 'ina@gmail.com', '$2y$10$q/4NTmieJ81bFPGVguusl.7/7iCmEEoatMXkZgZhR83UXe5C/uI76', 'pelanggan'),
 (11, 'lana', 'lanaza@gmail.com', '$2y$10$e2IK2LSVv1eFAWW1JQal2ejQhGLBwHUShBrTsf8gzbTAS5rnOgtc2', 'pelanggan');
