bc023108aa20d8b81de0ab9f7710ea175d2ce8cd
afrahmrtza <afrahmaritza18@gmail.com>
2026-06-13 20:30:31 +0800

    update

--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -26,66 +26,66 @@ class UserControllerTest extends TestCase
         $this->assertEquals(200, $result->getStatusCode());
     }
 
-    // public function testLoginGagal()
-    // {
-    //     $controller = new UserController();
+    public function testLoginGagal()
+    {
+        $controller = new UserController();
 
-    //     $requestFactory = new ServerRequestFactory();
-    //     $responseFactory = new ResponseFactory();
+        $requestFactory = new ServerRequestFactory();
+        $responseFactory = new ResponseFactory();
 
-    //     $request = $requestFactory->createServerRequest("POST", "/login");
-    //     $request = $request->withParsedBody([
-    //         'email' => 'afrah@gmail.com',
-    //         'password' => 'salah'
-    //     ]);
+        $request = $requestFactory->createServerRequest("POST", "/login");
+        $request = $request->withParsedBody([
+            'email' => 'afrah@gmail.com',
+            'password' => 'salah'
+        ]);
 
-    //     $response = $responseFactory->createResponse();
+        $response = $responseFactory->createResponse();
 
-    //     $result = $controller->login($request, $response);
+        $result = $controller->login($request, $response);
 
-    //     $this->assertEquals(401, $result->getStatusCode());
-    // }
+        $this->assertEquals(401, $result->getStatusCode());
+    }
 
-    // public function testRegisterBerhasil()
-    // {
-    //     $controller = new UserController();
+    public function testRegisterBerhasil()
+    {
+        $controller = new UserController();
 
-    //     $requestFactory = new \Slim\Psr7\Factory\ServerRequestFactory();
-    //     $responseFactory = new \Slim\Psr7\Factory\ResponseFactory();
+        $requestFactory = new \Slim\Psr7\Factory\ServerRequestFactory();
+        $responseFactory = new \Slim\Psr7\Factory\ResponseFactory();
 
-    //     $request = $requestFactory->createServerRequest("POST", "/register");
-    //     $request = $request->withParsedBody([
-    //         'nama' => 'maritzaa',
-    //         'email' => 'baru@mail.com',
-    //         'password' => '123456'
-    //     ]);
+        $request = $requestFactory->createServerRequest("POST", "/register");
+        $request = $request->withParsedBody([
+            'nama' => 'maritzaa',
+            'email' => 'baru@mail.com',
+            'password' => '123456'
+        ]);
 
-    //     $response = $responseFactory->createResponse();
+        $response = $responseFactory->createResponse();
 
-    //     $result = $controller->register($request, $response);
+        $result = $controller->register($request, $response);
 
-    //     $this->assertEquals(201, $result->getStatusCode());
-    // }
-    // public function testRegisterEmailDuplikat()
-    // {
-    // $controller = new UserController();
+        $this->assertEquals(201, $result->getStatusCode());
+    }
+    public function testRegisterEmailDuplikat()
+    {
+    $controller = new UserController();
 
-    // $requestFactory = new \Slim\Psr7\Factory\ServerRequestFactory();
-    // $responseFactory = new \Slim\Psr7\Factory\ResponseFactory();
+    $requestFactory = new \Slim\Psr7\Factory\ServerRequestFactory();
+    $responseFactory = new \Slim\Psr7\Factory\ResponseFactory();
 
-    // $request = $requestFactory->createServerRequest("POST", "/register");
-    // $request = $request->withParsedBody([
-    //     'nama' => 'Afrah',
-    //     'email' => 'afrah@gmail.com', // sudah ada
-    //     'password' => 'password123'
-    // ]);
+    $request = $requestFactory->createServerRequest("POST", "/register");
+    $request = $request->withParsedBody([
+        'nama' => 'Afrah',
+        'email' => 'afrah@gmail.com', // sudah ada
+        'password' => 'password123'
+    ]);
 
-    // $response = $responseFactory->createResponse();
+    $response = $responseFactory->createResponse();
 
-    // $result = $controller->register($request, $response);
+    $result = $controller->register($request, $response);
 
-    // $this->assertEquals(400, $result->getStatusCode());
-    // }
+    $this->assertEquals(400, $result->getStatusCode());
+    }
     
 }
 use PHPUnit\TextUI\Configuration\Php;
