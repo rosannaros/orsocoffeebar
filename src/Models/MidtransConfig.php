2c953c01589fede2daad6faab81d296f635d8c4d
marit <afrahmaritza18@gmail.com>
2026-06-17 14:03:49 +0800

    Delete src directory

--- src/Models/MidtransConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-
-class MidtransConfig {
-    public static $serverKey = 'Mid-server-Fv0j-OGf-rcrajTkL7HbF4EK';
-    public static $isProduction = false;
-    public static $isSanitized = true;
-    public static $is3ds = true;
-}
